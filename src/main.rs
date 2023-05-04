9a578c8e0ba34caae2972e999e030b83ca0f5d25
cyrex562 <cyrex562@gmail.com>
2023-05-04 18:58:53 -0400

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use dioxus::prelude::*;
+
+fn main() {
+    hot_reload_init!();
+    // println!("Hello, world!");
+    dioxus_desktop::launch(App);
+}
+
+fn App(cx: Scope) -> Element {
+    cx.render(rsx! {
+        div {
+            "Hello, world!"
+            div {
+                form {
+                    div {
+                        label{
+
+                        }
+                        input{
+                            name: "import_file_input",
+                            r#type: "file",
+                            onchange: move |evt| {
+                                println!("onchange: {:?}", evt);
+
+                            }
+                        }
+                    }
+                    div {
+                        button{
+                            onclick: move |event| {
+                        event.stop_propagation();
+                        }}
+                    }
+                }
+            }
+        }
+    })
+}
